066d9b3cccad9701ec3c2965ad5c039723bfa063
JoosepPodekrat <144919619+JoosepPodekrat@users.noreply.github.com>
2024-04-04 02:22:06 +0300

    Revert "Adde Joptionpane UI and basically finished the game logic"

--- Jaatisemang/src/Customer.java
+++ Jaatisemang/src/Customer.java
@@ -1,38 +1,29 @@
 import java.util.List;
 
 public class Customer {
-    private String likedIceCream;
-    private int WillingToBuy;
-    private double MaxPrice;
+    private  double money;
+    private List<String> likedIceCreams;
+    private  String name;
 
 
-Priit
-    public Customer(String likedIceCream,int WillingToBuy,double MaxPrice) {
-        this.likedIceCream = likedIceCream;
-        this.WillingToBuy = WillingToBuy;
-        this.MaxPrice = MaxPrice;
-
- Updated upstream
+<<<<<<< Updated upstream
     public Customer(double money, List<String> likedIceCreams, String name) {
         this.money = money;
         this.likedIceCreams = likedIceCreams;
         this.name = name;
-
+=======
     public Customer(String likedIceCream,int WillingToBuy,double MaxPrice) {
         this.likedIceCream = likedIceCream;// Milline jäätis meeldib
         this.WillingToBuy = WillingToBuy;// Mitu on nõus ostma korraga
         this.MaxPrice = MaxPrice;// Max hind, millega on nõus ostma
- Stashed changes
- main
+>>>>>>> Stashed changes
     }
 
-    public void buyIceCream(Player player){
-        player.Bought(likedIceCream,WillingToBuy,MaxPrice);
-    }
+    public void buyIceCream(IceCream iceCream){
+        this.money = this.money - iceCream.getCustomerPrice();
+        System.out.println("Customer "+this.name  + " Bought 1 ice cream");
+        //logic to remove 1 ice cream from player
 
-    @Override
-    public String toString(){
-        return likedIceCream+"on lemmik ja on nõus ostma "+WillingToBuy;
     }
 
 
